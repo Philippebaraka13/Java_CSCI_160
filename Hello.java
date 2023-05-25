115b82fdac616e27928fc6fce877024c3b38434b
Philippe <kamokamophilippe13@gmail.com>
2023-05-25 00:26:43 -0500

    hey

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 11;
+        b = 11;
+        c = a + b;
+        System.out.println("The value of c is " + c);
+        System.out.println("Hello");
+    }
+}
